ae941c0a509c4099d6367f03b4daa11291d894e4
Rahmatulah12 <blcak.gun123@gmail.com>
2021-01-21 18:34:57 +0700

    homepage done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-$(document).ready(function(){
-        
-  
-
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        items:5,
+        autoWidth: false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1350:{
+                items:4
+            }
+        }
+    });
 });
