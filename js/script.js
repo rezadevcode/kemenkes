70c0cebd24ab9bc834e1ae503c5f90baf13da66e
Rahmatulah12 <blcak.gun123@gmail.com>
2021-01-23 22:03:04 +0700

    revisi dot active slider text

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
         items:5,
         autoWidth: false,
         autoHeight: false,
+        dot: false,
         responsive:{
             0:{
                 items:1
@@ -36,15 +37,15 @@ showSlides(slideIndex = n);
 function showSlides(n) {
 var i;
 var slides = document.getElementsByClassName("mySlides");
-var dots = document.getElementsByClassName("dot");
+var ovals = document.getElementsByClassName("ovals");
 if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    for (i = 0; i < ovals.length; i++) {
+    ovals[i].className = ovals[i].className.replace(" active-slide", "");
     }
 slides[slideIndex-1].style.display = "block";
-dots[slideIndex-1].className += " active";
+ovals[slideIndex-1].className += " active-slide";
 }
